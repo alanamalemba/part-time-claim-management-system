33fd5252dc13318d385d682396e93743de0f2ee8
Alan Amalemba <alanlsamalemba@gmail.com>
2024-01-14 15:53:48 +0300

    fixed wrong password crashing server bug

--- server/routes/Login.js
+++ server/routes/Login.js
@@ -17,6 +17,7 @@ router.post("/", async (req, res) => {
   const passwordMatch = await bcrypt.compare(password, user.password);
   if (!passwordMatch) {
     res.json({ error: "Wrong username or password!" });
+    return;
   }
 
   const accessToken = sign({ email: user.email, id: user.id }, "secretKey");
